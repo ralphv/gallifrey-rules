eadadc19ecec3e4aad492c02ca0ba340365d18bf
Ralph Varjabedian <ralphv77@gmail.com>
2024-08-09 13:20:15 -0400

    adding favicon

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Gallifrey Rules',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
