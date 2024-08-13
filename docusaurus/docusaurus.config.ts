2521164c644e1d071308fc4f4143567b46035610
Ralph Varjabedian <ralphv77@gmail.com>
2024-08-12 20:59:13 -0400

    update docusaurus

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -46,8 +46,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/gallifrey-rules-social-card.jpg',
     navbar: {
       title: 'Gallifrey Rules',
       logo: {
